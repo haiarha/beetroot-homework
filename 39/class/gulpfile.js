b2992114b1c01d535af09ca8ed082de377ea493f
Vadym Veremeichyk <mydlenvadim@gmail.com>
2021-07-17 13:37:08 +0300

    Add autoprefixer

--- 39/class/gulpfile.js
+++ 39/class/gulpfile.js
@@ -1,22 +1,30 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const del = require('del');
-const sourcemaps = require('gulp-sourcemaps');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const del = require("del");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
 
 function cleanStyles() {
-  return del(['./css']);
+  return del(["./css"]);
 }
 
 function buildStyles() {
-  return gulp.src('./sass/*.scss')
+  return gulp
+    .src("./sass/*.scss")
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./css'));
-};
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+        grid: "autoplace",
+      })
+    )
+    .pipe(sourcemaps.write("./maps"))
+    .pipe(gulp.dest("./css"));
+}
 
 exports.watch = function () {
-  gulp.watch('./sass/**/*', buildStyles)
-}
+  gulp.watch("./sass/**/*", buildStyles);
+};
 
 exports.default = gulp.series(cleanStyles, buildStyles);
