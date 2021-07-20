2efa12a9784e3fbae901ff8df84a272003b59634
Vadym Veremeichyk <mydlenvadim@gmail.com>
2021-07-20 21:10:12 +0300

    Add all

---
+++ 40/class/src/index.js
@@ -0,0 +1,15 @@
+// import $ from 'jquery';
+
+// $('body').text('HI!');
+
+
+const test2 = () => {
+  console.log('12123123Hello world!');
+}
+
+const test = () => {
+  console.log('Hello world!');
+}
+
+test();
+test();
